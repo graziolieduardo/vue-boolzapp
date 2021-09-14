f143abbaf1711e59517ee0cf77d6f5e1e5e89697
Eduardo <graziolieduardo@gmail.com>
2021-09-14 11:35:41 +0200

    Aggiunta Menu a tendina

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 Vue.config.devtools = true;
 const app = new Vue({
     el: '#root',
-    data: {
+    data: { 
         search: '',
         message: '',
         activeContact: "",
@@ -32,11 +32,12 @@ const app = new Vue({
                 name: 'Fabio',
                 avatar: '_2',
                 visible: true,
-                messages: [{
-                    date: '20/03/2020 16:30:00',
-                    message: 'Ciao come stai?',
-                    status: 'sent'
-                },
+                messages: [
+                    {
+                        date: '20/03/2020 16:30:00',
+                        message: 'Ciao come stai?',
+                        status: 'sent'
+                    },
                     {
                         date: '20/03/2020 16:30:55',
                         message: 'Bene grazie! Stasera ci vediamo?',
@@ -53,11 +54,12 @@ const app = new Vue({
                 name: 'Samuele',
                 avatar: '_3',
                 visible: true,
-                messages: [{
-                    date: '28/03/2020 10:10:40',
-                    message: 'La Marianna va in campagna',
-                    status: 'received'
-                },
+                messages: [
+                    {
+                        date: '28/03/2020 10:10:40',
+                        message: 'La Marianna va in campagna',
+                        status: 'received'
+                    },
                     {
                         date: '28/03/2020 10:20:10',
                         message: 'Sicuro di non aver sbagliato chat?',
@@ -74,11 +76,12 @@ const app = new Vue({
                 name: 'Luisa',
                 avatar: '_4',
                 visible: true,
-                messages: [{
-                    date: '10/01/2020 15:30:55',
-                    message: 'Lo sai che ha aperto una nuova pizzeria?',
-                    status: 'sent'
-                },
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
+                    },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Si, ma preferirei andare al cinema',
@@ -115,7 +118,7 @@ const app = new Vue({
             );
         },
         getTime() {
-            return ( '0' + dayjs().date()).slice(-2) + '/' + ('0' + (dayjs().month() + 1)).slice(-2) + '/' + dayjs().year() + ' ' + ( '0' + dayjs().hour()).slice(-2) + ':' + ( '0' + dayjs().minute()).slice(-2) + ':' + ('0' + dayjs().second()).slice(-2);
+            return dayjs().format('DD/MM/YYYY hh:mm:ss' )
        },
        searchContact() {
            this.contacts.forEach((element) => {
@@ -130,6 +133,10 @@ const app = new Vue({
                     element.visible = true;
                 }
         }); 
+       },
+       openDropdownMenu(e) {
+            let menu = e.target.nextElementSibling;
+            menu.classList.toggle('show')
        }
     }
 });
